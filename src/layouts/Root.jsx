95fbae758d6cd0f35c409f5e0d781a3efe50b1d3
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-05-13 01:47:22 +0600

    navbar created

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,30 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './layouts/Root';
+import Home from './pages/Home/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
   },
 ]);
 
