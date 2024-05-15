21e9d8c194ab58f0b9dc7f353495acc6cfba4673
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-05-15 22:37:26 +0600

    verify token created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -8,11 +10,14 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: [
-      "http://localhost:5173",
+        'http://localhost:5173',
+        'https://libro-center.web.app',
+        'https://libro-center.firebaseapp.com'
     ],
     credentials: true,
-  }));
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k4ytc6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,6 +31,34 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+    console.log('log info:', req.method, req.url);
+    next();
+}
+
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware:', token);
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+const cookieOption = {
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
+    secure: process.env.NODE_ENV == "production" ? true : false,
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -35,7 +68,23 @@ async function run() {
         const borrowBooksCollection = client.db('libroCenter').collection('borrowedBooks');
 
 
-        app.get('/books', async (req, res) => {
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            // console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.cookie('token', token, cookieOption)
+                .send({ success: true });
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('loging out', user);
+            res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ success: true })
+        })
+
+
+        app.get('/books', logger, verifyToken, async (req, res) => {
+            // console.log('cooookies', req.cookies);
             const cursor = booksCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -43,8 +92,8 @@ async function run() {
 
         app.get('/allbooks', async (req, res) => {
             let query = {};
-            if(req.query?.category){
-                query = {category: req.query.category}
+            if (req.query?.category) {
+                query = { category: req.query.category }
             }
             const cursor = booksCollection.find(query);
             const result = await cursor.toArray();
@@ -110,10 +159,10 @@ async function run() {
         })
 
 
-        app.get('/borrowedbooks', async (req, res) =>{
+        app.get('/borrowedbooks', async (req, res) => {
             let query = {};
-            if(req.query?.email){
-                query = {email: req.query.email}
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await borrowBooksCollection.find(query).toArray();
             res.send(result);
@@ -126,10 +175,10 @@ async function run() {
         })
 
         app.delete('/borrowedbooks/:id', async (req, res) => {
-                const id = req.params.id;
-                const query = {_id: id};
-                const result = await borrowBooksCollection.deleteOne(query);
-                res.send(result);
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await borrowBooksCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
@@ -141,10 +190,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
-
 
 
 app.get('/', (req, res) => {
